4b7b6688678d375446b06e0328299fd2f7ea7d66
Truong <truongthanhng2000@gmail.com>
2024-08-26 15:02:38 +0700

    myapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  isCheckoutPage = false;
+
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isCheckoutPage = this.router.url === '/checkout';
+
+       
+       
+      }
+    });
+  }
 }
